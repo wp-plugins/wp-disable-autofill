99e61f3d20339dc9f9c902cf517053761040900e
amg26 <amg26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-19 19:40:29 +0000

    Restructuring...
    git-svn-id: https://plugins.svn.wordpress.org/wp-disable-autofill/trunk@1184414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-disable-autofill.php
+++ wp-disable-autofill.php
@@ -3,8 +3,8 @@
 /*
 * Plugin Name: WP Disable Autofill
 * Plugin URI: http://andrewmgunn.com
-* Description: Disables autofill for all input fields and forms.
-* Version: 1.1
+* Description: A lightweight plugin that disables the browser's ability to autofill forms and input tags on the client-side using JavaScript.
+* Version: 1.2
 * Author: Andrew M. Gunn
 * Author URI: http://andrewmgunn.com
 * License: GPL2
@@ -13,29 +13,13 @@
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
 
-function register_scripts() {
-	wp_register_script( 'wpda_script', plugins_url('js/wp-disable-autofill.js', __FILE__), array('jquery'));
-	wp_register_style( 'wdpa_style', plugins_url('css/wp-disable-autofill.css', __FILE__));
+function register_wpda_scripts() {
+	wp_register_script( 'wpda_script', plugins_url('inc/scripts.js', __FILE__), array('jquery'));
+	wp_register_style( 'wdpa_style', plugins_url('inc/styles.css', __FILE__));
 	wp_enqueue_script( 'wpda_script' );
 	wp_enqueue_style( 'wdpa_style' );
 }
 
-function disable_autofill() { ?>
-	
-	<script type="text/javascript">
-		//alert("disable");
-
-		//get php content variable, run the function from the JS file on it
-		//and return the content variable if using the content filter
-	</script>
-	
-
-<?php }
-
-//to make a filter, add phph function that contains javascript and apply it
-//to the_content. this would be needed if im doing a settings pages or some kind
-//of admin stuff thats going to need the code to NOT just run on every page
-
-add_action( 'wp_enqueue_scripts', 'register_scripts' );
+add_action( 'wp_enqueue_scripts', 'register_wpda_scripts' );
 //add_filter( 'the_content', 'disable_autofill')
 ?>
