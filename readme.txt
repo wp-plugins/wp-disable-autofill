f634d837921040be30641f129e894b0234af059a
amg26 <amg26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 13:23:03 +0000

    Recommit
    git-svn-id: https://plugins.svn.wordpress.org/wp-disable-autofill/trunk@1189588 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== WP Disable Autofill ===
+Donate link: http://andrewmgunn.com/donate/
+Contributors: amg26
+Tags: autofill, form autofill, input autofill, disable autofill, disable form autofill
+Requires at least: 3.5
+Tested up to: 4.2.2
+Stable tag: 1.3.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Disable the browser's ability to autofill forms and input fields. Ideal for forms with sensitive information and provides extra level of form submission security.
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+==Readme Generator==
+
+This Readme file was generated using <a href = 'http://sudarmuthu.com/wordpress/wp-readme'>wp-readme</a>, which generates readme files for WordPress Plugins.
---
+++ wp-disable-autofill.php
@@ -0,0 +1,25 @@
+<?php
+/*
+* Plugin Name: WP Disable Autofill
+* Plugin URI: http://andrewmgunn.com/wp-disable-autofill/
+* Description: Disable the browser's ability to autofill forms and input fields. Ideal for forms with sensitive information and provides extra level of form submission security.
+* Version: 1.3.1
+* Author: Andrew M. Gunn
+* Author URI: http://andrewmgunn.com
+* Text Domain: wp-disable-autofill
+* License: GPL2
+*/
+
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+require_once('options/options.php');
+
+add_action( 'wp_enqueue_scripts', 'register_wpda_scripts' );
+
+
+
+function register_wpda_scripts() {
+	wp_register_script( 'wpda_script', plugins_url('inc/plugin-scripts.js', __FILE__), array('jquery'));
+	wp_register_style( 'wdpa_style', plugins_url('inc/plugin-styles.css', __FILE__));
+	wp_enqueue_script( 'wpda_script' );
+	wp_enqueue_style( 'wdpa_style' );
+}
